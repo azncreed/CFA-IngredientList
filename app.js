569a94f89ab55b2616cd54d0c4edd3dc09da0934
Sylvia Shen <azncreedii@gmail.com>
2017-05-15 10:54:40 +1000

    trying to deploy in heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var url = process.env.MONGOLAB_URI;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
